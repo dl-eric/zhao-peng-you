3b182ea53f6807fb8a3933f0b197ee28d77d8c29
David Wang <davidw1339@gmail.com>
2020-10-10 17:36:50 -0400

    main menu

--- frontend/src/views/mainMenu/index.jsx
+++ frontend/src/views/mainMenu/index.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+import Button from 'components/button';
 
 export default function() {
-    return <div>MAIN MENU</div>
+    return (
+        <div className="main-menu">
+            <h1>ZHAO PENG YOU / 找朋友</h1>
+            <Link to="/create">
+                <Button>CREATE GAME</Button>
+            </Link>
+            <Link to="/join">
+                <Button>JOIN GAME</Button>
+            </Link>
+        </div>
+    )
 }
