efc3cf4cd1c3ec8bd1c38d5e4e44d0aa4873f68d
Eric <ericdl712@gmail.com>
2020-10-10 17:02:00 -0400

    Initial commit

---
+++ backend/api/api.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+#from .endpoints import note, utils
+
+api_router = APIRouter()
+#api_router.include_router(note.router, prefix="/note", tags=['Note'])
+#api_router.include_router(utils.router, prefix="/utils", tags=['Utils'])
---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from api import api
+from config import origins
+
+# FastAPI
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api.api_router)
